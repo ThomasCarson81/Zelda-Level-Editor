7be42de63d6b1582be1fc7b769d7a8c484adb468
Thomas Carson <thomas.carson2024@gmail.com>
2025-06-02 22:47:49 +0100

    Added project files
    The Editor will be made entirely in raylib-cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Raylib_cs;
+
+namespace Editor;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 600, "Editor");
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.DarkGray);
+            Raylib.DrawText("This is a test", 300, 250, 32, Color.White);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
